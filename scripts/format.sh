f2398a38a4c34e74242374de15130c2263658562
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-11-22 19:11:12 -0500

    Update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -15,7 +15,7 @@
 until [ -d .git ]; do cd ..; done
 
 # Run Prettier on the staged files
-yarn --cwd "frontend" pretty-quick --staged
+yarn run --cwd "frontend" pretty-quick --staged
 
 # If Black is installed, run it on the staged files.  (Black requires
 # Python 3.6+, but you can reformat Python 2 code with it).
