1165cfbe6f4d3f4ceb80247c7b5f229d7891e739
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-12-07 22:26:54 +0400

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -43,17 +43,5 @@ module.exports = {
 
       return webpackConfig
     },
-    plugins: [
-      // Hide critical dependency warnings from Webpack, as CircleCI treats them as errors.
-      // https://medium.com/tomincode/hiding-critical-dependency-warnings-from-webpack-c76ccdb1f6c1
-      // Remove after updating bokehjs to 2.0.0
-      // https://github.com/bokeh/bokeh/issues/9594#issuecomment-577227353
-      new webpack.ContextReplacementPlugin(/\/bokehjs\//, data => {
-        for (let i in data.dependencies) {
-          delete data.dependencies[i].critical
-        }
-        return data
-      }),
-    ],
   },
 }
